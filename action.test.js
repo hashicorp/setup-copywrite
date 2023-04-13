2cc6b1d400e5622e3e9a658e19c4119dd1227090
Caleb Albers <7110138+CalebAlbers@users.noreply.github.com>
2023-04-13 12:29:44 -0700

    Update to use default GHA token (#21)
    * Update to use default GHA token
    
    * use v3 action/checkout
    
    * skip failing tests
    
    ---------
    
    Co-authored-by: dlaguerta <dianne@hashicorp.com>

--- action.test.js
+++ action.test.js
@@ -41,7 +41,7 @@ beforeAll(() => {
 })
 
 beforeEach(() => {
-  process.env['INPUT_GITHUB-TOKEN'] = 'testtoken'
+  process.env.INPUT_TOKEN = 'testtoken'
   process.env.INPUT_VERSION = 'latest'
   process.env['INPUT_VERSION-CHECKSUM'] = '5663389ef1a8ec48af6ca622e66bf0f54ba8f22c127f14cb8a3f429e40868582'
 
@@ -56,9 +56,11 @@ describe('action', () => {
     const scopeAPI = nock('https://api.github.com')
       .get('/repos/hashicorp/copywrite/releases/latest')
       .reply(200, mockRelease)
-    const scopeWeb = nock('https://github.com')
-      .get('/hashicorp/copywrite/releases/download/v0.1.3/copywrite_0.1.3_windows_x86_64.zip')
+      .get('/repos/hashicorp/copywrite/releases/assets/3')
       .replyWithFile(200, path.resolve(__dirname, 'test.zip'), { 'content-type': 'application/octet-stream' })
+    // const scopeWeb = nock('https://github.com')
+    //   .get('/hashicorp/copywrite/releases/download/v0.1.3/copywrite_0.1.3_windows_x86_64.zip')
+    //   .replyWithFile(200, path.resolve(__dirname, 'test.zip'), { 'content-type': 'application/octet-stream' })
     const spyCoreAddPath = jest.spyOn(core, 'addPath')
     const spyCoreSetOutput = jest.spyOn(core, 'setOutput')
 
@@ -73,7 +75,6 @@ describe('action', () => {
       const action = require('./action')
       await expect(await action()).resolves
       expect(scopeAPI.isDone()).toBeTruthy()
-      expect(scopeWeb.isDone()).toBeTruthy()
       expect(spyCoreAddPath).toHaveBeenCalled()
       expect(spyCoreSetOutput).toHaveBeenCalledWith('version', 'v0.1.3')
       done()
@@ -84,9 +85,11 @@ describe('action', () => {
     const scopeAPI = nock('https://api.github.com')
       .get('/repos/hashicorp/copywrite/releases/tags/v0.1.3')
       .reply(200, mockRelease)
-    const scopeWeb = nock('https://github.com')
-      .get('/hashicorp/copywrite/releases/download/v0.1.3/copywrite_0.1.3_windows_x86_64.zip')
+      .get('/repos/hashicorp/copywrite/releases/assets/3')
       .replyWithFile(200, path.resolve(__dirname, 'test.zip'), { 'content-type': 'application/octet-stream' })
+    // const scopeWeb = nock('https://github.com')
+    //   .get('/hashicorp/copywrite/releases/download/v0.1.3/copywrite_0.1.3_windows_x86_64.zip')
+    //   .replyWithFile(200, path.resolve(__dirname, 'test.zip'), { 'content-type': 'application/octet-stream' })
     const spyCoreAddPath = jest.spyOn(core, 'addPath')
     const spyCoreSetOutput = jest.spyOn(core, 'setOutput')
 
@@ -102,88 +105,87 @@ describe('action', () => {
       const action = require('./action')
       await expect(await action()).resolves
       expect(scopeAPI.isDone()).toBeTruthy()
-      expect(scopeWeb.isDone()).toBeTruthy()
       expect(spyCoreAddPath).toHaveBeenCalled()
       expect(spyCoreSetOutput).toHaveBeenCalledWith('version', 'v0.1.3')
       done()
     })
   })
 
-//   test('retries transient errors', (done) => {
-//     const scope = nock('https://api.github.com')
-//       .get('/repos/hashicorp/copywrite/releases/tags/v0.1.3')
-//       .reply(500, 'expected transient error')
-//       .get('/repos/hashicorp/copywrite/releases/tags/v0.1.3')
-//       .reply(200, mockRelease)
-//       .get('/repos/hashicorp/copywrite/releases/assets/3')
-//       .replyWithFile(200, path.resolve(__dirname, 'test.zip'), { 'content-type': 'application/octet-stream' })
-//
-//     fs.mkdtemp(path.join(os.tmpdir(), 'setup-copywrite-'), async (err, directory) => {
-//       if (err) throw err
-//
-//       process.env.INPUT_VERSION = 'v0.1.3'
-//       process.env.RUNNER_TEMP = directory
-//
-//       const spyOsHomedir = jest.spyOn(os, 'homedir')
-//       spyOsHomedir.mockReturnValue(directory)
-//
-//       const action = require('./action')
-//       await expect(await action()).resolves
-//       expect(scope.isDone()).toBeTruthy()
-//       done()
-//     })
-//   })
-//
-//   test('retries abuse limit errors', (done) => {
-//     const scope = nock('https://api.github.com')
-//       .get('/repos/hashicorp/copywrite/releases/tags/v0.1.3')
-//       .reply(403, {
-//         message: 'You have triggered an abuse detection mechanism and have been temporarily blocked from content creation. Please retry your request again later.',
-//         documentation_url: 'https://docs.github.com/rest/overview/resources-in-the-rest-api#abuse-rate-limits'
-//       })
-//       .get('/repos/hashicorp/copywrite/releases/tags/v0.1.3')
-//       .reply(200, mockRelease)
-//       .get('/repos/hashicorp/copywrite/releases/assets/3')
-//       .replyWithFile(200, path.resolve(__dirname, 'test.zip'), { 'content-type': 'application/octet-stream' })
-//
-//     fs.mkdtemp(path.join(os.tmpdir(), 'setup-copywrite-'), async (err, directory) => {
-//       if (err) throw err
-//
-//       process.env.INPUT_VERSION = 'v0.1.3'
-//       process.env.RUNNER_TEMP = directory
-//
-//       const spyOsHomedir = jest.spyOn(os, 'homedir')
-//       spyOsHomedir.mockReturnValue(directory)
-//
-//       const action = require('./action')
-//       await expect(await action()).resolves
-//       expect(scope.isDone()).toBeTruthy()
-//       done()
-//     })
-//   })
-//
-//   test('retries rate limit errors', (done) => {
-//     const scope = nock('https://api.github.com')
-//       .get('/repos/hashicorp/copywrite/releases/tags/v0.1.3')
-//       .reply(429, 'expected rate limit error')
-//       .get('/repos/hashicorp/copywrite/releases/tags/v0.1.3')
-//       .reply(200, mockRelease)
-//       .get('/repos/hashicorp/copywrite/releases/assets/3')
-//       .replyWithFile(200, path.resolve(__dirname, 'test.zip'), { 'content-type': 'application/octet-stream' })
-//
-//     fs.mkdtemp(path.join(os.tmpdir(), 'setup-copywrite-'), async (err, directory) => {
-//       if (err) throw err
-//
-//       process.env.INPUT_VERSION = 'v0.1.3'
-//       process.env.RUNNER_TEMP = directory
-//
-//       const spyOsHomedir = jest.spyOn(os, 'homedir')
-//       spyOsHomedir.mockReturnValue(directory)
-//
-//       const action = require('./action')
-//       await expect(await action()).resolves
-//       expect(scope.isDone()).toBeTruthy()
-//       done()
-//     })
-//   })
+  test('retries transient errors', (done) => {
+    const scope = nock('https://api.github.com')
+      .get('/repos/hashicorp/copywrite/releases/tags/v0.1.3')
+      .reply(500, 'expected transient error')
+      .get('/repos/hashicorp/copywrite/releases/tags/v0.1.3')
+      .reply(200, mockRelease)
+      .get('/repos/hashicorp/copywrite/releases/assets/3')
+      .replyWithFile(200, path.resolve(__dirname, 'test.zip'), { 'content-type': 'application/octet-stream' })
+
+    fs.mkdtemp(path.join(os.tmpdir(), 'setup-copywrite-'), async (err, directory) => {
+      if (err) throw err
+
+      process.env.INPUT_VERSION = 'v0.1.3'
+      process.env.RUNNER_TEMP = directory
+
+      const spyOsHomedir = jest.spyOn(os, 'homedir')
+      spyOsHomedir.mockReturnValue(directory)
+
+      const action = require('./action')
+      await expect(await action()).resolves
+      expect(scope.isDone()).toBeTruthy()
+      done()
+    })
+  })
+  // TODO: fix failing tests
+  test.skip('retries abuse limit errors', (done) => {
+    const scope = nock('https://api.github.com')
+      .get('/repos/hashicorp/copywrite/releases/tags/v0.1.3')
+      .reply(403, {
+        message: 'You have triggered an abuse detection mechanism and have been temporarily blocked from content creation. Please retry your request again later.',
+        documentation_url: 'https://docs.github.com/rest/overview/resources-in-the-rest-api#abuse-rate-limits'
+      })
+      .get('/repos/hashicorp/copywrite/releases/tags/v0.1.3')
+      .reply(200, mockRelease)
+      .get('/repos/hashicorp/copywrite/releases/assets/3')
+      .replyWithFile(200, path.resolve(__dirname, 'test.zip'), { 'content-type': 'application/octet-stream' })
+
+    fs.mkdtemp(path.join(os.tmpdir(), 'setup-copywrite-'), async (err, directory) => {
+      if (err) throw err
+
+      process.env.INPUT_VERSION = 'v0.1.3'
+      process.env.RUNNER_TEMP = directory
+
+      const spyOsHomedir = jest.spyOn(os, 'homedir')
+      spyOsHomedir.mockReturnValue(directory)
+
+      const action = require('./action')
+      await expect(await action()).resolves
+      expect(scope.isDone()).toBeTruthy()
+      done()
+    })
+  })
+
+  test.skip('retries rate limit errors', (done) => {
+    const scope = nock('https://api.github.com')
+      .get('/repos/hashicorp/copywrite/releases/tags/v0.1.3')
+      .reply(429, 'expected rate limit error')
+      .get('/repos/hashicorp/copywrite/releases/tags/v0.1.3')
+      .reply(200, mockRelease)
+      .get('/repos/hashicorp/copywrite/releases/assets/3')
+      .replyWithFile(200, path.resolve(__dirname, 'test.zip'), { 'content-type': 'application/octet-stream' })
+
+    fs.mkdtemp(path.join(os.tmpdir(), 'setup-copywrite-'), async (err, directory) => {
+      if (err) throw err
+
+      process.env.INPUT_VERSION = 'v0.1.3'
+      process.env.RUNNER_TEMP = directory
+
+      const spyOsHomedir = jest.spyOn(os, 'homedir')
+      spyOsHomedir.mockReturnValue(directory)
+
+      const action = require('./action')
+      await expect(await action()).resolves
+      expect(scope.isDone()).toBeTruthy()
+      done()
+    })
+  })
 })
